f9dd1fb9c1555e0754551dff672de8d300530840
OSHAN JR <79368912+0shanjr@users.noreply.github.com>
2024-01-12 23:48:03 +0530

    updated

--- res/js/script.js
+++ res/js/script.js
@@ -7,20 +7,23 @@ $('.navTrigger').click(function () {
 });
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    let prevScrollPos = window.pageYOffset;
-    const header = document.querySelector('nav');
+// document.addEventListener('DOMContentLoaded', function () {
+//     let prevScrollPos = window.pageYOffset;
+//     const header = document.querySelector('nav');
   
-    window.onscroll = function() {
-      const currentScrollPos = window.pageYOffset;
-      if (prevScrollPos > currentScrollPos) {
-        header.style.top = '0';
-      } else {
-        header.style.top = `-${header.offsetHeight}px`;
-      }
-      prevScrollPos = currentScrollPos;
-    };
-  });
+//     window.onscroll = function() {
+//       const currentScrollPos = window.pageYOffset;
+//       if (prevScrollPos > currentScrollPos) {
+//         header.style.top = '0';
+//       } else {
+//         header.style.top = `-${header.offsetHeight}px`;
+//       }
+//       prevScrollPos = currentScrollPos;
+//     };
+//   });
+
+
+
   
 
 
